5d62f0124f09bdfadef44c7a8663e03d2e0187f5
Felipe Ubuntu 18 <felipe.candido8@gmail.com>
2020-05-20 17:30:00 -0300

    Subindo aplicação em desenvolviemento

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+/**
+ * formidable trata os arquivos enviados via upload
+ * ele trata todas os dados, mas é mais completo para arquivos
+ * 
+ */
+var formdable = require('formidable'); // npm install formidable --save
+
+router.delete('/file', (req,res)=>{
+
+  let form = new formdable.IncomingForm({
+
+    uploadDir: './upload', // define o diretório onde será salvo os arquivos
+    keepExtensions: true // mantém a extensão do(s) arquivo(s) sem isso salva sem extensão
+
+  }) //IncomingForm chama o formulário 
+
+  form.parse(req, (err,fields,files)=>{
+    // interpreta os dados 
+    
+    res.json({
+      fields
+    });
+
+
+  })
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', (req,res)=> {
+
+  let form = new formdable.IncomingForm({
+
+    uploadDir: './upload', // define o diretório onde será salvo os arquivos
+    keepExtensions: true // mantém a extensão do(s) arquivo(s) sem isso salva sem extensão
+
+  }) //IncomingForm chama o formulário 
+
+  form.parse(req, (err,fields,files)=>{
+    // interpreta os dados 
+    
+    res.json({
+      files
+    });
+
+  }) 
+});
+
+module.exports = router;
